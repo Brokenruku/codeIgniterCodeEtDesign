5efb1974beac9cd571fefb2d67c228aafb7c1c6d
Brokenruku <168399819+Brokenruku@users.noreply.github.com>
2026-04-28 11:37:08 +0300

    ajout email

--- base/base.sql
+++ base/base.sql
@@ -23,9 +23,11 @@ CREATE TABLE plats (
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
--- Version sans clés étrangères pour démarrer
+
 CREATE TABLE user_interactions (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
