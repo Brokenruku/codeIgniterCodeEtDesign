a44ee66eb5c0990d7c61ff252ea15ead98bc021d
Brokenruku <168399819+Brokenruku@users.noreply.github.com>
2026-04-28 09:15:33 +0300

    a

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,6 +5,6 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/home', 'Home::index');
 $routes->get('/login', 'loginController::index');
 $routes->get('/inscription', 'inscriptionController::index');
